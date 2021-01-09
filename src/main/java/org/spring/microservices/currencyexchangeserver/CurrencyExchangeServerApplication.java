9c957e40b641cb56e05c355112ef7e30a36ca7d7
Rakesh Nair <Rakesh.Nair@accelya.local>
2021-01-09 16:14:56 +0530

    Added currency exchange service

--- src/main/java/org/spring/microservices/currencyexchangeserver/CurrencyExchangeServerApplication.java
+++ src/main/java/org/spring/microservices/currencyexchangeserver/CurrencyExchangeServerApplication.java
@@ -2,8 +2,10 @@ package org.spring.microservices.currencyexchangeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.spring.microservices")
 public class CurrencyExchangeServerApplication {
 
 	public static void main(String[] args) {
